0693c280a03c31d40fcd1caeac18917d5e0d171e
sanjeet kumar <skc0845@gmail.com>
2023-09-14 04:10:48 +0530

    added database model

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
 dotenv.config()
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
+
+
+//mongoose database model
+const User = mongoose.model('user', {
+    name: String,
+    email: String,
+    mobile: Number,
+    password: String,
+})
+
 
 app.get('/', (req, res) => {
     res.send("Welcome to the portal")
