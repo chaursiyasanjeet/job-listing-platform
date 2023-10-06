8a84eac96ba9da84d3239446ae8fd02c92740e17
sanjeet kumar <skc0845@gmail.com>
2023-10-06 14:51:28 +0530

    enhcnecment:naming of job api

--- server.js
+++ server.js
@@ -1,57 +1,55 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const dotenv = require('dotenv')
-const mongoose = require("mongoose")
-const auth = require("./routes/auth")
-const addjob = require("./routes/addJob")
-const jwt = require("jsonwebtoken")
-dotenv.config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const auth = require("./routes/auth");
+const job = require("./routes/job");
+const jwt = require("jsonwebtoken");
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/health', (req, res) => {
-    res.status(200).json({
-        service: "job-listing-server",
-        status: "Active",
-        time: new Date(),
-    })
-})
-
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    service: "job-listing-server",
+    status: "Active",
+    time: new Date(),
+  });
+});
 
 //routes
-app.use(auth)
-app.use(addjob)
-
+app.use(auth);
+app.use(job);
 
 //error handler middleware
 app.use((req, res, next) => {
-    const err = new Error('page not found');
-    err.status = 404;
-    next(err)
-})
+  const err = new Error("page not found");
+  err.status = 404;
+  next(err);
+});
 
 //error handler
 app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-        error: {
-            status: err.status || 500,
-            message: err.message
-        }
-    })
-})
+  res.status(err.status || 500).json({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
 
 //server listening
 app.listen(process.env.PORT, (error) => {
-    mongoose
-        .connect(process.env.MONGODB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        .then(
-            console.log(`server is running on http://localhost:${process.env.PORT}`)
-        )
-        .catch((err) => console.log(err))
-})
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(
+      console.log(`server is running on http://localhost:${process.env.PORT}`)
+    )
+    .catch((err) => console.log(err));
+});
