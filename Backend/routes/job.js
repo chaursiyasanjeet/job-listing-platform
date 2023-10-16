ad5cf33fd953284e175ff50ade44a785185ce12d
sanjeet kumar <skc0845@gmail.com>
2023-10-16 09:11:31 +0530

    arranged all file in backend folder

--- Backend/routes/job.js
+++ Backend/routes/job.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Job = require("../models/job");
 const isLoggedIn = require("../middleware/isLoggedIn");
+const { findById, find } = require("../models/user");
 
 router.post("/addjob", isLoggedIn, async (req, res, next) => {
   try {
@@ -88,10 +89,46 @@ router.put("/editJob/:id", isLoggedIn, async (req, res) => {
       },
     });
     res.status(200).json({
-      message: "Job detsils updated",
+      message: "Job details updated",
     });
   } catch (error) {
     console.log(error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
+
+router.get("/getjobs", async (req, res) => {
+  try {
+    const jobs = await Job.find(
+      { skills: "react", jobType: "Full time" },
+      {
+        companyName: 1,
+        logoUrl: 1,
+        jobPosition: 1,
+        jobType: 1,
+        mode: 1,
+        location: 1,
+        skills: 1,
+      }
+    );
+    res.status(200).json({ data: jobs });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.get("/jobdetails/:id", async (req, res) => {
+  try {
+    const jobdetails = await Job.findById(req.params.id);
+    if (!jobdetails) {
+      return res.status(404).json({ error: "Job details not found" });
+    }
+    res.status(200).json({ data: jobdetails });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 module.exports = router;
