b4453107196c7eeafedc021033719bbd05258b85
sanjeet kumar <skc0845@gmail.com>
2023-10-18 16:55:18 +0530

    added registration page and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Registration from "../src/page/Registration/Registration";
+import Signin from "./page/Signin/Signin";
 function App() {
-  return <div className="App">sijnk</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Registration />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/src/page/Signin/Signin.js
@@ -0,0 +1,7 @@
+import styles from "./Signin.module.css";
+
+const Signin = () => {
+  return <div>sigin page</div>;
+};
+
+export default Signin;
