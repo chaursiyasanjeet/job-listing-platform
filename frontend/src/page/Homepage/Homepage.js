75c1cd12eb0ad3ecf577d5dd697040c61b2524a9
sanjeet kumar <skc0845@gmail.com>
2023-11-04 02:20:12 +0530

    added bug fix

--- frontend/src/page/Homepage/Homepage.js
+++ frontend/src/page/Homepage/Homepage.js
@@ -1,31 +1,45 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Jobsearch from "../../components/Jobsearch/Jobserach";
 import JobContainer from "../../components/JobContainer/JobContainer";
+import { useState, useEffect } from "react";
 import { jobsearch } from "../../apis/job";
 
-const result = await jobsearch();
-
 const Homepage = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const result = await jobsearch();
+        if (result && result.jobs) {
+          setJobs(result.jobs);
+        }
+      } catch (error) {
+        console.error("Error fetching jobs:", error);
+      }
+    };
+
+    fetchJobs();
+  }, []);
   return (
     <>
       <Navbar />
       <Jobsearch />
-      {result &&
-        result.jobs.map((item, index) => {
-          return (
-            <JobContainer
-              companyName={item.companyName}
-              jobPosition={item.jobPosition}
-              jobType={item.jobType}
-              logoUrl={item.logoUrl}
-              mode={item.mode}
-              skills={item.skills}
-              id={item._id}
-              salary={item.salary}
-              key={index}
-            />
-          );
-        })}
+      {jobs.map((item, index) => {
+        return (
+          <JobContainer
+            companyName={item.companyName}
+            jobPosition={item.jobPosition}
+            jobType={item.jobType}
+            logoUrl={item.logoUrl}
+            mode={item.mode}
+            skills={item.skills}
+            id={item._id}
+            salary={item.salary}
+            key={index}
+          />
+        );
+      })}
     </>
   );
 };
