1e3bfdb66993b43de702032f6a687dd73fc155d2
sanjeet kumar <skc0845@gmail.com>
2023-11-04 19:34:03 +0530

    bug fix

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,6 @@ const cors = require("cors");
 dotenv.config();
 const app = express();
 
-app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
